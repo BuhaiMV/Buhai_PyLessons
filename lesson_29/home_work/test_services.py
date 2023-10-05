6737e07965121c515baa6761896c7b3a507247d4
BuhaiMV <maksym.buhai@gmail.com>
2023-10-05 21:40:22 +0300

    update home work lesson 29

--- lesson_29/home_work/test_services.py
+++ lesson_29/home_work/test_services.py
@@ -5,7 +5,7 @@ dataBase = DataBase()
 endpoint = Endpoint()
 
 
-def test_get_object():
+def test_1():
     post_response = endpoint.post_request({
         "name": "Apple MacBook Pro 16",
         "data": {
@@ -16,14 +16,14 @@ def test_get_object():
         }
     })
     get_response = endpoint.get_request(post_response['id'])
-    add_data_response = dataBase.add_to_db({"data": get_response})
-    get_data_response = dataBase.get_data_from_db(add_data_response.inserted_id)['data']
+    add_data_in_db = dataBase.add_to_db({"data": get_response})
+    get_data_from_db = dataBase.get_data_from_db(add_data_in_db.inserted_id)['data']
     print(get_response)
-    print(get_data_response)
-    assert get_response == get_data_response
+    print(get_data_from_db)
+    assert get_response == get_data_from_db
 
 
-def test_get_object():
+def test_2():
     post_response = endpoint.post_request({
         "name": "Apple MacBook Pro 16",
         "data": {
@@ -33,7 +33,8 @@ def test_get_object():
             "Hard disk size": "1 TB"
         }
     })
-    add_data_response = dataBase.add_to_db({"data": post_response})
+    add_data_to_db = dataBase.add_to_db({"data": post_response})
+
     put_response = endpoint.put_request(post_response['id'], {
         "name": "Apple MacBook Pro 16",
         "data": {
@@ -45,8 +46,38 @@ def test_get_object():
         }
     })
     get_response = endpoint.get_request(put_response['id'])
-    update_data_response = dataBase.update_in_db(add_data_response.inserted_id, {"data": get_response})
-    get_data_response = dataBase.get_data_from_db(add_data_response.inserted_id)['data']
+    update_data_in_db = dataBase.update_in_db(add_data_to_db.inserted_id, {"data": get_response})
+    get_data_from_db = dataBase.get_data_from_db(update_data_in_db.inserted_id)['data']
     print(get_response)
-    print(get_data_response)
-    assert get_response == get_data_response
+    print(get_data_from_db)
+    assert get_response == get_data_from_db
+
+
+def test_3():
+    add_data_to_db = dataBase.add_to_db({"data": {
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+    }})
+    post_response = endpoint.post_request(add_data_to_db['data'])
+
+    update_data_in_db = dataBase.update_in_db(add_data_to_db.inserted_id, {"data": {
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 2049.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB",
+            "color": "silver"
+        }
+    }})
+    get_data_in_db = dataBase.get_data_from_db(add_data_to_db.inserted_id)['data']
+    put_response = endpoint.put_request(post_response['id'], get_data_in_db)
+    get_response = endpoint.get_request(put_response['id'])
+    print(get_response)
+    print(get_data_in_db)
+    assert get_response == get_data_in_db
